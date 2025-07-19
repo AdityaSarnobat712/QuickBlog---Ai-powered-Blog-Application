df96fcafc3a955d57625569b6c499952daa28fb8
AdityaSarnobat712 <adityasarnobat24@gmail.com>
2025-07-19 11:29:43 +0530

    fix: correct import path for Newsletter component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Bloglist from '../components/Bloglist'
-import NewsLetter from '../components/NewsLetter'
+import NewsLetter from '../components/Newsletter'
 import Footer from '../components/Footer'
 
 const Home = () => {
